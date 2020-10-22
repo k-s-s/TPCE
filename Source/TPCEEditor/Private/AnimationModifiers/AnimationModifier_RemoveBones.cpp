8c4d3092d680ab8c3c83c62c2ecc18d97809b2f9
greisane <ggreisane@gmail.com>
2020-10-22 15:22:21 -0300

    Add AnimationModifier_RemoveBones Animation Modifier to remove sets of bones.

---
+++ Source/TPCEEditor/Private/AnimationModifiers/AnimationModifier_RemoveBones.cpp
@@ -0,0 +1,94 @@
+// This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+
+#include "AnimationModifiers/AnimationModifier_RemoveBones.h"
+
+#include "Animation/AnimSequence.h"
+#include "UObject/UObjectBaseUtility.h"
+#include "ExtraTypes.h"
+
+UAnimationModifier_RemoveBones::UAnimationModifier_RemoveBones()
+{
+	bCaseInsensitive = true;
+}
+
+void UAnimationModifier_RemoveBones::OnApply_Implementation(UAnimSequence* AnimationSequence)
+{
+	Super::OnApply_Implementation(AnimationSequence);
+
+	FString Path = GetPathNameSafe(AnimationSequence);
+	if (PathFilter != NAME_None && !Path.Contains(PathFilter.ToString()))
+	{
+		return;
+	}
+
+	// Copy bone filters with or without case and remove the '+' prefix
+	TArray<FWildcardString> BoneWildcards;
+	const int32 NumFilters = BoneFilters.Num();
+	BoneWildcards.Reserve(NumFilters);
+	for (const FString& BoneFilter : BoneFilters)
+	{
+		const FString NoPrefix = BoneFilter.StartsWith(TEXT("+")) ? BoneFilter.RightChop(1) : BoneFilter;
+		BoneWildcards.Add(bCaseInsensitive ? NoPrefix.ToLower() : NoPrefix);
+	}
+
+	// Find indices of all the bones that match the filters
+	TMap<int32, bool> MatchingBones;
+	USkeleton* Skeleton = AnimationSequence->GetSkeleton();
+	if (Skeleton)
+	{
+		const FReferenceSkeleton& RefSkeleton = Skeleton->GetReferenceSkeleton();
+
+		// Bones are in strictly increasing order
+		// Adding a bone to MatchingBones with True will knock out the entire branch
+		const int32 NumBones = RefSkeleton.GetNum();
+
+		for (int32 BoneIndex = 0; BoneIndex < NumBones; BoneIndex++)
+		{
+			const int32 ParentBoneIndex = RefSkeleton.GetParentIndex(BoneIndex);
+			if (bool* bWithChildren = MatchingBones.Find(ParentBoneIndex))
+			{
+				if (*bWithChildren)
+				{
+					// Parent bone matched and included children, so add this and include subsequent children
+					MatchingBones.Add(BoneIndex, true);
+					continue;
+				}
+			}
+
+			FString BoneName = RefSkeleton.GetBoneName(BoneIndex).ToString();
+			if (bCaseInsensitive)
+			{
+				BoneName.ToLowerInline();
+			}
+
+			for (int32 FilterIndex = 0; FilterIndex < NumFilters; FilterIndex++)
+			{
+				if (BoneWildcards[FilterIndex].IsMatch(BoneName))
+				{
+					bool bIncludesChildren = BoneFilters[FilterIndex].StartsWith(TEXT("+"));
+					MatchingBones.Add(BoneIndex, bIncludesChildren);
+					if (bIncludesChildren)
+					{
+						// No need to keep matching
+						break;
+					}
+				}
+			}
+		}
+
+		// Can't call UAnimSequence::RemoveTrack directly, so unfortunately UAnimationBlueprintLibrary has to be relied on
+		// Keep in mind GetAnimationTrackNames returns the actual array, so we must iterate backwards while removing
+		const TArray<FName>& TrackNames = AnimationSequence->GetAnimationTrackNames();
+		for (int32 TrackIndex = TrackNames.Num() - 1; TrackIndex >= 0; --TrackIndex)
+		{
+			const FName TrackName = TrackNames[TrackIndex];
+			const int32 BoneIndex = RefSkeleton.FindBoneIndex(TrackName);
+			if (MatchingBones.Contains(BoneIndex) != bInvertSelection)
+			{
+				UAnimationBlueprintLibrary::RemoveBoneAnimation(AnimationSequence, TrackName, /*bIncludeChildren=*/false, /*bFinalize=*/false);
+			}
+		}
+
+		UAnimationBlueprintLibrary::FinalizeBoneAnimation(AnimationSequence);
+	}
+}
---
+++ Source/TPCEEditor/Private/AnimationModifiers/AnimationModifier_RemoveBones.h
@@ -0,0 +1,48 @@
+// This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimationModifier.h"
+#include "Animation/AnimTypes.h"
+#include "Misc/WildcardString.h"
+
+#include "AnimationModifier_RemoveBones.generated.h"
+
+class UAnimSequence;
+
+/**
+ * Animation Modifier to remove sets of bones.
+ */
+UCLASS(meta = (DisplayName="Remove Bones"))
+class UAnimationModifier_RemoveBones : public UAnimationModifier
+{
+	GENERATED_BODY()
+
+public:
+	UAnimationModifier_RemoveBones();
+
+public:
+	/** Only animations with a path containing this filter as a case insensitive substring will be affected. Empty value matches all. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Settings)
+	FName PathFilter;
+
+	/**
+	 * An array of filters specifying the bones that should be removed.
+	 * Wildcards '?' and '*' are allowed. Prefix with '+' to include children.
+	 *
+	 * Examples: "+head", "hair_*", "twist_??_L"
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Settings)
+	TArray<FString> BoneFilters;
+
+	/** If True, bone filters ignore case. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Settings)
+	bool bCaseInsensitive;
+
+	/** Invert the result. If True, the list specifies the bones that are KEPT. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Settings)
+	bool bInvertSelection;
+
+	virtual void OnApply_Implementation(UAnimSequence* AnimationSequence) override;
+};
