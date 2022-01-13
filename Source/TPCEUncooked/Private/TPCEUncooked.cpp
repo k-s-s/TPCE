26c93a624bd28c55371ac1a4e213cb66b351b714
greisane <ggreisane@gmail.com>
2022-01-12 21:23:53 -0300

    All modules are PreDefault now, which fixes a weird race condition?

--- Source/TPCEUncooked/Private/TPCEUncooked.cpp
+++ Source/TPCEUncooked/Private/TPCEUncooked.cpp
@@ -2,4 +2,23 @@
 
 #include "TPCEUncooked.h"
 
-IMPLEMENT_MODULE(FDefaultModuleImpl, TPCEUncooked)
+class FTPCEUncookedModule : public IModuleInterface
+{
+	// Begin IModuleInterface
+	virtual void StartupModule() override;
+	// End IModuleInterface
+};
+
+IMPLEMENT_MODULE(FTPCEUncookedModule, TPCEUncooked);
+
+void FTPCEUncookedModule::StartupModule()
+{
+	// Not sure if necessary. See https://forums.unrealengine.com/t/4-24-deprecation-of-developer-module-type-uncookedonly-not-working-on-nativized-bp-assets/134758
+	/*
+	const FString LongName = FPackageName::ConvertToLongScriptPackageName(TEXT("TPCEUncooked"));
+	if (UPackage* Package = Cast<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), nullptr, *LongName, false, false)))
+	{
+		Package->SetPackageFlags(PKG_EditorOnly);
+	}
+	*/
+}
