d0b6ca89aba09e3a7190043b6a879fe652427a8d
greisane <ggreisane@gmail.com>
2022-10-21 05:20:40 -0300

    Add ground overlay to ATopDownPlayerController

--- Source/TPCE/Private/GameFramework/TopDownPlayerController.cpp
+++ Source/TPCE/Private/GameFramework/TopDownPlayerController.cpp
@@ -2,25 +2,38 @@
 
 #include "GameFramework/TopDownPlayerController.h"
 
-#include "GameFramework/Pawn.h"
-#include "Components/ArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/ArmComponent.h"
 #include "Components/SphereComponent.h"
-#include "GameFramework/TopDownPushToTargetComponent.h"
 #include "Components/InputComponent.h"
+#include "Components/DecalComponent.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/TopDownPushToTargetComponent.h"
 #include "Engine/EngineTypes.h"
 #include "Engine/World.h"
 #include "Engine/CollisionProfile.h"
+#include "Engine/Canvas.h"
 #include "DrawDebugHelpers.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogTopDownPlayerController, Log, All);
 
 ATopDownPlayerController::ATopDownPlayerController(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
+	, bGroundOverlay(false)
+	, OverlayZ(5.f)
+	, OverlayHeight(20.f)
+	, OverlayCanvasSize(1024)
+	, OverlayPixelsPerUnit(1.f)
 {
+	// Player controllers are normally invisible, but this unnecessarily complicates things
+	// If it becomes invisible again for some reason, check that ADebugCameraController::OnDeactivate isn't at fault
+	SetHidden(false);
+#if WITH_EDITORONLY_DATA
+	bHiddenEd = false;
+#endif // WITH_EDITORONLY_DATA
 	bShowMouseCursor = true;
 
-	// Create camera mout
+	// Create camera mount
 	static const FName CameraMountName(TEXT("CameraMount"));
 	CameraMount = CreateDefaultSubobject<USphereComponent>(CameraMountName);
 	CameraMount->SetupAttachment(RootComponent);
@@ -33,9 +46,13 @@ ATopDownPlayerController::ATopDownPlayerController(const FObjectInitializer& Obj
 	const int32 ProfilesCount = CollisionProfiles.GetNumOfProfiles();
 	bool bIsProfileValid = false;
 	for (int32 i = 0; !bIsProfileValid && i < ProfilesCount; ++i)
+	{
 		bIsProfileValid = CollisionProfiles.GetProfileByIndex(i)->Name == CameraMountCollisionProfileName;
+	}
 	if (!bIsProfileValid)
+	{
 		UE_LOG(LogTopDownPlayerController, Warning, TEXT("Collision profile not found: '%s'. Camera mount created with default collision settings."), *CameraMountCollisionProfileName.ToString());
+	}
 #endif
 
 	CameraMount->SetCollisionProfileName(CameraMountCollisionProfileName);
@@ -77,6 +94,17 @@ ATopDownPlayerController::ATopDownPlayerController(const FObjectInitializer& Obj
 	CameraTractor->bAdjustTargetLagForViewTarget = true;
 	CameraTractor->SetUpdatedComponent(CameraMount);
 	CameraTractor->SetIsReplicated(false);
+
+	// Create overlay decal
+	static const FName GroundOverlayName(TEXT("GroundOverlay"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> OverlayMaterialObject(TEXT("Material'/TPCE/M_TopDownPlayerControllerOverlay.M_TopDownPlayerControllerOverlay'"));
+	GroundOverlay = CreateDefaultSubobject<UDecalComponent>(GroundOverlayName);
+	GroundOverlay->SetupAttachment(GetRootComponent());
+	GroundOverlay->SetAbsolute(true, true, true);
+	GroundOverlay->SetRelativeRotation(FRotator(-90.f, 0.f, 0.f));
+	GroundOverlay->SetDecalMaterial(OverlayMaterialObject.Object);
+	GroundOverlay->SetVisibility(false);
+	GroundOverlay->bDestroyOwnerAfterFade = false;
 }
 
 void ATopDownPlayerController::ForceAbsolute(USceneComponent* SceneComponent)
@@ -110,6 +138,27 @@ void ATopDownPlayerController::BeginPlay()
 	Super::BeginPlay();
 
 	CameraMount->SetWorldLocation(RootComponent->GetComponentLocation());
+
+	// Create overlay render target texture to be drawn to
+	OverlayCanvasRenderTarget = UCanvasRenderTarget2D::CreateCanvasRenderTarget2D(this, UCanvasRenderTarget2D::StaticClass(), 512, 512);
+	if (OverlayCanvasRenderTarget)
+	{
+		OverlayCanvasRenderTarget->ClearColor = FLinearColor::Transparent;
+		OverlayCanvasRenderTarget->OnCanvasRenderTargetUpdate.AddDynamic(this, &ATopDownPlayerController::DrawGroundOverlay);
+
+		if (UMaterialInstanceDynamic* OverlayMID = GroundOverlay->CreateDynamicMaterialInstance())
+		{
+			static const FName OverlayMaterialTextureParameterName(TEXT("RenderTarget"));
+			OverlayMID->SetTextureParameterValue(OverlayMaterialTextureParameterName, OverlayCanvasRenderTarget);
+		}
+	}
+}
+
+void ATopDownPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	UpdateGroundOverlay();
 }
 
 void ATopDownPlayerController::CalcCamera(float DeltaTime, FMinimalViewInfo& OutResult)
@@ -133,5 +182,62 @@ bool ATopDownPlayerController::HasActivePawnControlCameraComponent() const
 void ATopDownPlayerController::SetCameraTargetComponent(USceneComponent* NewTargetComponent, const FName& SocketName)
 {
 	if (CameraTractor)
+	{
 		CameraTractor->SetTargetComponent(NewTargetComponent, SocketName);
+	}
+}
+
+void ATopDownPlayerController::UpdateGroundOverlay()
+{
+	bool bShowOverlay = false;
+
+	if (bGroundOverlay && bViewExtentsValid)
+	{
+		FVector CanvasSize = (ViewExtentsMax - ViewExtentsMin) * OverlayPixelsPerUnit;
+		FVector CanvasScale = FVector(1.f / OverlayPixelsPerUnit, 1.f / OverlayPixelsPerUnit, 1.f);
+		if (CanvasSize.X != OverlayCanvasSize)
+		{
+			CanvasScale.X /= OverlayCanvasSize / CanvasSize.X;
+			CanvasSize.X = OverlayCanvasSize;
+		}
+		if (CanvasSize.Y != OverlayCanvasSize)
+		{
+			CanvasScale.Y /= OverlayCanvasSize / CanvasSize.Y;
+			CanvasSize.Y = OverlayCanvasSize;
+		}
+
+		FVector OverlayOrigin = (ViewExtentsMin + ViewExtentsMax) * .5f;
+		OverlayOrigin.Z = OverlayZ;
+		const FVector CanvasOrigin = OverlayOrigin * FVector(1.f, 1.f, 0.f) - (CanvasSize * CanvasScale * .5f);
+		OverlayCanvasTransform = FScaleRotationTranslationMatrix(CanvasScale, FRotator::ZeroRotator, CanvasOrigin).Inverse();
+
+		// Reposition decal
+		const FTransform DecalTransform = FTransform(FRotator(-90.f, 0.f, 0.f), OverlayOrigin, FVector(1.f, CanvasScale.Y, CanvasScale.X));
+		GroundOverlay->SetWorldTransform(DecalTransform);
+		GroundOverlay->DecalSize = FVector(OverlayHeight, CanvasSize.Y * .5f, CanvasSize.X * .5f);
+
+		if (CanvasSize.X > 0 && CanvasSize.Y > 0 && OverlayCanvasRenderTarget)
+		{
+			// Finally render if everything is in order
+			OverlayCanvasRenderTarget->ResizeTarget(CanvasSize.X, CanvasSize.Y);
+			OverlayCanvasRenderTarget->UpdateResource();
+			bShowOverlay = true;
+		}
+	}
+
+	GroundOverlay->SetVisibility(bShowOverlay);
+}
+
+void ATopDownPlayerController::DrawGroundOverlay(UCanvas* InCanvas, int32 Width, int32 Height)
+{
+	InCanvas->Canvas->PushRelativeTransform(OverlayCanvasTransform);
+
+	if (!IsPendingKill() && OnOverlayUpdate.IsBound())
+	{
+		OnOverlayUpdate.Broadcast(InCanvas, Width, Height);
+	}
+
+	ReceiveOverlayUpdate(InCanvas, Width, Height);
+
+	InCanvas->Canvas->PopTransform();
 }
